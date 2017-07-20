7cceb26e776d8c0562a60cda194ddd9a2456b783
Benjamin van Zwienen <benjaminvanzwienen@live.nl>
2017-07-20 20:13:41 +0200

    Add routing and update navbar

--- ng-src/src/app/app.module.ts
+++ ng-src/src/app/app.module.ts
@@ -2,20 +2,30 @@ import { AuthService } from './auth.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { OverviewComponent } from './overview/overview.component';
+import { AddComponent } from './add/add.component';
+
+const appRoutes: Routes = [
+  { path: "", component: OverviewComponent },
+  { path: "add", component: AddComponent },
+  { path: "**", component: OverviewComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    OverviewComponent
+    OverviewComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
