11fc90488b84fdf588b79c5988452d2f15f880a9
Benjamin van Zwienen <benjaminvanzwienen@live.nl>
2017-07-20 11:06:50 +0200

    Setup navbar

--- ng-src/src/app/app.module.ts
+++ ng-src/src/app/app.module.ts
@@ -1,16 +1,21 @@
+import { AuthService } from './auth.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { OverviewComponent } from './overview/overview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    OverviewComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
